038c57d2adc6cea2080064534d091f6cab6c18a4
iGilga <pyua87@gmail.com>
2015-12-29 01:38:20 +0300

    Импорт частично SuperBug.Politrange.Crawler для консоли.

---
+++ src/RatingMarker.CrawlerConsole/AutofacConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using RatingMarker.Crawler.Workflow;
+
+namespace RatingMarker.CrawlerConsole
+{
+    public class AutofacConfig
+    {
+         public IContainer Build()
+         {
+             var builder = new ContainerBuilder();
+
+             RegisterCrawler(builder);
+
+             return builder.Build();
+         }
+
+        public void RegisterCrawler(ContainerBuilder builder)
+        {
+            builder.RegisterModule<CrawlerModule>();
+        }
+    }
+}
---
+++ src/RatingMarker.CrawlerConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Autofac;
+using RatingMarker.Crawler.Workflow;
+
+namespace RatingMarker.CrawlerConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConsoleConfigure();
+
+            AutofacConfig autofacConfig = new AutofacConfig();
+
+            using (var scope = autofacConfig.Build())
+            {
+                var crawlerManage = scope.Resolve<CrawlerManage>();
+
+                crawlerManage.InitializeCrawler();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ConsoleConfigure()
+        {
+            Console.WindowWidth = 120;
+            Console.BufferWidth = 120;
+        }
+    }
+}
