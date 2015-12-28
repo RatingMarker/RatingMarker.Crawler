ed845d99d8deba29cca7a669c9efab6f3b219d8a
iGilga <pyua87@gmail.com>
2015-12-29 01:37:25 +0300

    Импорт SuperBug.Politrange.Crawler

---
+++ src/RatingMarker.Crawler.Workflow/Parsers/SitemapParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace RatingMarker.Crawler.Workflow.Parsers
+{
+    public class SitemapParser: IParser
+    {
+        public IEnumerable<string> GetUrls(string content)
+        {
+            IList<string> urls = new List<string>();
+
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+
+                xml.LoadXml(content);
+
+                XmlNamespaceManager manager = new XmlNamespaceManager(xml.NameTable);
+
+                manager.AddNamespace("s", "http://www.sitemaps.org/schemas/sitemap/0.9");
+                XmlNodeList xnList = GetNodes(xml, manager, "/s:sitemapindex/s:sitemap");
+
+                if (xnList.Count > 0)
+                {
+                    foreach (XmlNode node in xnList)
+                    {
+                        var url = node["loc"].InnerText;
+                        urls.Add(url);
+                    }
+                }
+
+                xnList = GetNodes(xml, manager, "/s:urlset/s:url");
+
+                if (xnList.Count > 0)
+                {
+                    foreach (XmlNode node in xnList)
+                    {
+                        var url = node["loc"].InnerText;
+                        urls.Add(url);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            urls = urls.Distinct().ToList();
+
+            urls = RemoveExtGzipUrls(urls).ToList();
+
+            return urls;
+        }
+
+        private IEnumerable<string> RemoveExtGzipUrls(IEnumerable<string> urls)
+        {
+            var removeExt = ".gz".ToCharArray();
+
+            return urls.Select(x => x.TrimEnd(removeExt));
+        }
+
+        private XmlNodeList GetNodes(XmlDocument xml, XmlNamespaceManager manager, string tags)
+        {
+            return xml.SelectNodes(tags, manager);
+        }
+    }
+}
---
+++ src/RatingMarker.Crawler.Workflow/Services/CrawlerPersonRankService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Autofac.Extras.NLog;
+using HtmlAgilityPack;
+using RatingMarker.Crawler.Data.Repositories;
+using RatingMarker.Crawler.Models;
+
+namespace RatingMarker.Crawler.Workflow.Services
+{
+    public interface ICrawlerPersonRankService
+    {
+        IEnumerable<PersonPageRank> GetPersonPageRanks(KeyValuePair<Page, string> page);
+    }
+
+    public class CrawlerPersonRankService: ICrawlerPersonRankService
+    {
+        private readonly ILogger logger;
+        private readonly IPersonRepository personRepository;
+
+        public CrawlerPersonRankService(IPersonRepository personRepository, ILogger logger)
+        {
+            this.personRepository = personRepository;
+            this.logger = logger;
+        }
+
+        public IEnumerable<PersonPageRank> GetPersonPageRanks(KeyValuePair<Page, string> page)
+        {
+            IList<PersonPageRank> ranks = new List<PersonPageRank>();
+
+            try
+            {
+                HtmlDocument htmlDocument = new HtmlDocument();
+
+                htmlDocument.LoadHtml(page.Value);
+
+                var nodes = htmlDocument.DocumentNode.SelectNodes("//p");
+
+                if (nodes != null)
+                {
+                    var persons = GetPersons();
+
+                    var text = string.Empty;
+
+                    foreach (Person person in persons)
+                    {
+                        foreach (Keyword keyword in person.Keywords)
+                        {
+                            foreach (HtmlNode node in nodes)
+                            {
+                                text = node.InnerText;
+                                if (text.Contains(keyword.Name))
+                                {
+                                    AddOrUpdateRank(ranks, page.Key, person);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.Message);
+            }
+
+            return ranks;
+        }
+
+        private IEnumerable<Person> GetPersons()
+        {
+            return personRepository.GetAll();
+        }
+
+        private void AddOrUpdateRank(IList<PersonPageRank> ranks, Page page, Person person)
+        {
+            var rank = ranks.SingleOrDefault(x => x.PersonId == person.PersonId);
+
+            if (rank == null)
+            {
+                rank = new PersonPageRank() {PageId = page.PageId, PersonId = person.PersonId};
+                ranks.Add(rank);
+            }
+
+            rank.Rank ++;
+        }
+
+        private bool IsExtensionUrl(string url)
+        {
+            return Path.HasExtension(url);
+        }
+    }
+}
